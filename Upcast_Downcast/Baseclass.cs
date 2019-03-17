429c5fd741a6bd2e93fb6d4d90fb816ff181b999
ArmenHovhannisyan1998 <48031430+ArmenHovhannisyan1998@users.noreply.github.com>
2019-03-17 05:22:29 +0400

    Add project files.

---
+++ Upcast_Downcast/Baseclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upcast_Downcast
+{
+    class Baseclass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method from BaseClass");
+        }
+    }
+}
---
+++ Upcast_Downcast/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upcast_Downcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derivedclass instance = new Derivedclass();
+            instance.Method();
+            Console.WriteLine(new string('-',50));
+
+            //Upcast
+
+            Baseclass instanceUP = (Baseclass)instance;
+            instanceUP.Method();
+            Console.WriteLine(new string('-', 50));
+
+            //Downcast
+
+            Derivedclass instanceDAWN = (Derivedclass)instanceUP;
+            instanceDAWN.Method();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
